34b7f45082a9af1b875a0ac80be1d973581160a1
apierson5 <60145227+apierson5@users.noreply.github.com>
2022-04-03 02:25:01 -0700

    added firebase app,store and components dir.

---
+++ components/sections/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer: React.FunctionComponent = () => {
+	return <div className="text-sky-500">This is the footer component</div>;
+};
+
+export default Footer;
---
+++ firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyChv186_NxqJ9qQv_2suB44_gdni33xSVc",
+	authDomain: "docs-universal.firebaseapp.com",
+	projectId: "docs-universal",
+	storageBucket: "docs-universal.appspot.com",
+	messagingSenderId: "295276405931",
+	appId: "1:295276405931:web:a4c8206a399b3ab01583ee",
+	measurementId: "G-EQ6EJ2TCWD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db, analytics };
