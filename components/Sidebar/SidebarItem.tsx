47d66e1290ebbb1ca34a905527da654e2f4495ea
apierson5 <60145227+apierson5@users.noreply.github.com>
2022-04-06 02:45:47 -0700

    removed linkDestination prop

--- components/Sidebar/SidebarItem.tsx
+++ components/Sidebar/SidebarItem.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 interface SidebarItemProps {
 	name?: string;
 	Icon?: JSX.Element;
-	linkDestination?: string;
 }
 
 export default function SidebarItem(props: SidebarItemProps) {
-	const { name, Icon, linkDestination } = props;
+	const { name, Icon } = props;
 
 	return (
 		<div className="group flex items-center justify-left gap-4 pl-2 py-2 text-indigo-100 hover:bg-indigo-800 hover:text-gray-100 focus:bg-green-500 rounded-lg cursor-pointer transition ease-in-out duration-250">
