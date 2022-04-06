96a271030603c6a5b2f778c33791a8124733768e
apierson5 <60145227+apierson5@users.noreply.github.com>
2022-04-06 01:27:50 -0700

    added firebase configs to .env.local

--- config/firebase.ts
+++ config/firebase.ts
@@ -16,15 +16,14 @@ import { SupportedAuthProvider } from "../types/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// TODO: Add configs to .env.local
 const firebaseConfig = {
-	apiKey: "AIzaSyChv186_NxqJ9qQv_2suB44_gdni33xSVc",
-	authDomain: "docs-universal.firebaseapp.com",
-	projectId: "docs-universal",
-	storageBucket: "docs-universal.appspot.com",
-	messagingSenderId: "295276405931",
-	appId: "1:295276405931:web:a4c8206a399b3ab01583ee",
-	measurementId: "G-EQ6EJ2TCWD",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
