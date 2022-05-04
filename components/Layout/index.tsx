5a950b3feba7b0a656ad8ecab39d702e456a9944
apierson5 <60145227+apierson5@users.noreply.github.com>
2022-05-03 19:14:23 -0700

    update Layout component to take up minimum height of screen

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,8 +4,8 @@ interface LayoutProps {
 	children: React.ReactElement;
 }
 
-const Layout: React.FC<LayoutProps> = (props) => {
-	return <div className="flex bg-slate-900 min-h-screen">{props.children}</div>;
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+	return <div className="flex bg-slate-900 min-h-screen">{children}</div>;
 };
 
 export default Layout;
