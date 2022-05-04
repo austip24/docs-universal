72d1c4230d7f3249a08ca244399dbd231860955c
apierson5 <60145227+apierson5@users.noreply.github.com>
2022-05-03 19:13:51 -0700

    added account page

---
+++ pages/account/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface AccountProps {}
+
+const Account: React.FC<AccountProps> = (props) => {
+	return <div className="text-indigo-50">Account</div>;
+};
+
+export default Account;
